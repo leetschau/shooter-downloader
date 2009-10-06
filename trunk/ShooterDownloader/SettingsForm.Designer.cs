26b36aa582c250b0ba8152198fc3c71ff15de0c5
pcfung <pcfung@84246662-b026-11de-8bdf-3374eb5c7316>
2009-10-06 16:46:57 +0000

    1. Removes EncodingTools from the solution. 2. Adjusts form size.

--- trunk/ShooterDownloader/SettingsForm.Designer.cs
+++ trunk/ShooterDownloader/SettingsForm.Designer.cs
@@ -43,9 +43,9 @@
             // chkEnableLog
             // 
             this.chkEnableLog.AutoSize = true;
-            this.chkEnableLog.Location = new System.Drawing.Point(12, 147);
+            this.chkEnableLog.Location = new System.Drawing.Point(12, 159);
             this.chkEnableLog.Name = "chkEnableLog";
-            this.chkEnableLog.Size = new System.Drawing.Size(96, 16);
+            this.chkEnableLog.Size = new System.Drawing.Size(98, 17);
             this.chkEnableLog.TabIndex = 0;
             this.chkEnableLog.Text = "啟動日誌功能";
             this.chkEnableLog.UseVisualStyleBackColor = true;
@@ -54,9 +54,9 @@
             // btnOk
             // 
             this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOk.Location = new System.Drawing.Point(186, 259);
+            this.btnOk.Location = new System.Drawing.Point(184, 254);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.Size = new System.Drawing.Size(75, 25);
             this.btnOk.TabIndex = 2;
             this.btnOk.Text = "確定";
             this.btnOk.UseVisualStyleBackColor = true;
@@ -65,9 +65,9 @@
             // btnCancel
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCancel.Location = new System.Drawing.Point(267, 259);
+            this.btnCancel.Location = new System.Drawing.Point(270, 254);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(75, 25);
             this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "取消";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -75,9 +75,9 @@
             // 
             // btnOpenLogFolder
             // 
-            this.btnOpenLogFolder.Location = new System.Drawing.Point(133, 143);
+            this.btnOpenLogFolder.Location = new System.Drawing.Point(133, 155);
             this.btnOpenLogFolder.Name = "btnOpenLogFolder";
-            this.btnOpenLogFolder.Size = new System.Drawing.Size(144, 23);
+            this.btnOpenLogFolder.Size = new System.Drawing.Size(144, 25);
             this.btnOpenLogFolder.TabIndex = 4;
             this.btnOpenLogFolder.Text = "開啟日誌所在資料夾";
             this.btnOpenLogFolder.UseVisualStyleBackColor = true;
@@ -86,44 +86,44 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 26);
+            this.label1.Location = new System.Drawing.Point(10, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 12);
+            this.label1.Size = new System.Drawing.Size(94, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "最大同時下載數:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 64);
+            this.label2.Location = new System.Drawing.Point(10, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.Size = new System.Drawing.Size(79, 13);
             this.label2.TabIndex = 6;
             this.label2.Text = "電影檔副檔名";
             // 
             // txtVideoFileExt
             // 
-            this.txtVideoFileExt.Location = new System.Drawing.Point(12, 95);
+            this.txtVideoFileExt.Location = new System.Drawing.Point(12, 103);
             this.txtVideoFileExt.Name = "txtVideoFileExt";
-            this.txtVideoFileExt.Size = new System.Drawing.Size(328, 22);
+            this.txtVideoFileExt.Size = new System.Drawing.Size(328, 20);
             this.txtVideoFileExt.TabIndex = 8;
             this.txtVideoFileExt.TextChanged += new System.EventHandler(this.txtVideoFileExt_TextChanged);
             // 
             // cbConcurrenctNum
             // 
             this.cbConcurrenctNum.FormattingEnabled = true;
-            this.cbConcurrenctNum.Location = new System.Drawing.Point(108, 23);
+            this.cbConcurrenctNum.Location = new System.Drawing.Point(108, 25);
             this.cbConcurrenctNum.Name = "cbConcurrenctNum";
-            this.cbConcurrenctNum.Size = new System.Drawing.Size(44, 20);
+            this.cbConcurrenctNum.Size = new System.Drawing.Size(44, 21);
             this.cbConcurrenctNum.TabIndex = 9;
             this.cbConcurrenctNum.SelectedIndexChanged += new System.EventHandler(this.cbConcurrenctNum_SelectedIndexChanged);
             // 
             // chkEnableConvert
             // 
             this.chkEnableConvert.AutoSize = true;
-            this.chkEnableConvert.Location = new System.Drawing.Point(12, 181);
+            this.chkEnableConvert.Location = new System.Drawing.Point(12, 196);
             this.chkEnableConvert.Name = "chkEnableConvert";
-            this.chkEnableConvert.Size = new System.Drawing.Size(156, 16);
+            this.chkEnableConvert.Size = new System.Drawing.Size(158, 17);
             this.chkEnableConvert.TabIndex = 10;
             this.chkEnableConvert.Text = "自動將簡體字幕轉成繁體";
             this.chkEnableConvert.UseVisualStyleBackColor = true;
@@ -134,7 +134,7 @@
             this.lblTitleVersion.AutoSize = true;
             this.lblTitleVersion.Font = new System.Drawing.Font("PMingLiU", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lblTitleVersion.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.lblTitleVersion.Location = new System.Drawing.Point(194, 221);
+            this.lblTitleVersion.Location = new System.Drawing.Point(12, 236);
             this.lblTitleVersion.Name = "lblTitleVersion";
             this.lblTitleVersion.Size = new System.Drawing.Size(65, 12);
             this.lblTitleVersion.TabIndex = 11;
@@ -142,9 +142,10 @@
             // 
             // SettingsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(357, 294);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(357, 308);
             this.ControlBox = false;
             this.Controls.Add(this.lblTitleVersion);
             this.Controls.Add(this.chkEnableConvert);
