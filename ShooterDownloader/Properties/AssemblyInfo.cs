b39709ce1daa22d5d8598b70b80a586055104eab
pcfung <pcfung@84246662-b026-11de-8bdf-3374eb5c7316>
2009-10-07 17:24:44 +0000

    1. Removes AssemblyFileVersion and AssemblyInformationalVersion since the product version and file version will always be the same as the assembly version in this project. 2. Adjusts settings in the setup project.

--- ShooterDownloader/Properties/AssemblyInfo.cs
+++ ShooterDownloader/Properties/AssemblyInfo.cs
@@ -33,5 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.1.2")]
-[assembly: AssemblyFileVersion("1.0.1.2")]
-[assembly: AssemblyInformationalVersion("1.0.1.2")]
+
+// No need to assign AssemblyFileVersion and AssemblyInformationalVersion
+//  if the product version and file version are always the same as the assembly version.
+//[assembly: AssemblyFileVersion("1.0.1.2")]
+//[assembly: AssemblyInformationalVersion("1.0.1.2")]
