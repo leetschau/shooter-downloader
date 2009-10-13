6f204116582fbd08b619bb892025f3ebc22ade98
pcfung <pcfung@84246662-b026-11de-8bdf-3374eb5c7316>
2009-10-13 04:13:42 +0000

    1. Updates build_installer.bat to automatically create 7z package. 2. Rename installer.nsi to installer.nsi.tmpl to avoid confusion.

--- Installer/build_installer.bat
+++ Installer/build_installer.bat
@@ -1,2 +1,4 @@
-VerSync -bin="..\ShooterDownloader\bin\Release\ShooterDownloader.exe" -in=installer.nsi -out=out.nsi
-"%NSIS_PATH%\makensis" out.nsi
+VerSync -bin="..\ShooterDownloader\bin\Release\ShooterDownloader.exe" -in=installer.nsi.tmpl -out=installer.nsi
+"%NSIS_PATH%\makensis" installer.nsi
+VerSync -bin="..\ShooterDownloader\bin\Release\ShooterDownloader.exe" -in=build_7z.bat.tmpl -out=build_7z.bat
+build_7z.bat
