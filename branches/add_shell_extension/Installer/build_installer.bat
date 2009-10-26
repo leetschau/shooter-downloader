2e2c3a360556c43cc038611cbe41291809884841
pcfung <pcfung@84246662-b026-11de-8bdf-3374eb5c7316>
2009-10-26 03:10:40 +0000

    Removes merged feature branch.

--- branches/add_shell_extension/Installer/build_installer.bat
+++
@@ -1,4 +0,0 @@
-VerSync -bin="..\ShooterDownloader\bin\Release\ShooterDownloader.exe" -in=installer.nsi.tmpl -out=installer.nsi
-"%NSIS_PATH%\makensis" installer.nsi
-VerSync -bin="..\ShooterDownloader\bin\Release\ShooterDownloader.exe" -in=build_7z.bat.tmpl -out=build_7z.bat
-build_7z.bat
